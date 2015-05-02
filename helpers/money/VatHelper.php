b204ebbf3dc7eabc26029b9c2719ded63cf0f1b1
Jiří Svoboda <jiri.svoboda@dlds.cz>
2015-05-02 13:56:47 +0200

    Added VatHelper

---
+++ helpers/money/VatHelper.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace dlds\components\helpers\money;
+
+class VatHelper {
+
+    /**
+     * Adds vat to given price and retrieves price with vat
+     * @param float $priceWithoutVat given price without vat
+     * @param float $vat vat percentage value to be added
+     */
+    public static function addVat($priceWithoutVat, $vat)
+    {
+        return $priceWithoutVat * (1 + $vat / 100);
+    }
+
+    /**
+     * Removes vat from given price and retrieves price without vat
+     * @param float $priceWithVat given price with vat
+     * @param float $vat vat percentage value to be removed
+     */
+    public static function subVat($priceWithVat, $vat)
+    {
+        return $priceWithVat / (1 + $vat / 100);
+    }
+}
