20e677184122bd90118eebb3f900ef9e2c4397e8
Jiří Svoboda <jiri.svoboda@dlds.cz>
2015-08-16 12:23:11 +0200

    fix subVat - now defined ratio is used

--- helpers/money/VatHelper.php
+++ helpers/money/VatHelper.php
@@ -2,7 +2,16 @@
 
 namespace dlds\components\helpers\money;
 
+use yii\helpers\ArrayHelper;
+
 class VatHelper {
+
+    /**
+     * @var array hold vat rations to sub given vat from value
+     */
+    protected static $ratios = [
+        '21' => 0.1736,
+    ];
 
     /**
      * Adds vat to given price and retrieves price with vat
@@ -13,7 +22,7 @@ class VatHelper {
     {
         if ($vat)
         {
-            return $priceWithoutVat * (1 + $vat / 100);
+            return $priceWithoutVat * (1 + ($vat / 100));
         }
 
         return $priceWithoutVat;
@@ -22,12 +31,19 @@ class VatHelper {
     /**
      * Removes vat from given price and retrieves price without vat
      * @param float $priceWithVat given price with vat
-     * @param float $vat vat percentage value to be removed
+     * @param float $vat vat percentage value to be removed (21% => 21)
      */
     public static function subVat($priceWithVat, $vat)
     {
         if ($vat)
         {
+            $ratio = ArrayHelper::getValue(self::$ratios, (string) $vat, false);
+
+            if ($ratio)
+            {
+                return $priceWithVat - ($priceWithVat * $ratio);
+            }
+
             return $priceWithVat / (1 + $vat / 100);
         }
 
