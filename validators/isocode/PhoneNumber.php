c0ee57433d941ff58a393a1070b7e9942d18e620
Jiří Svoboda <svobik7@gmail.com>
2017-02-20 11:21:46 +0100

    + PhoneNumber idd

--- validators/isocode/PhoneNumber.php
+++ validators/isocode/PhoneNumber.php
@@ -2,29 +2,51 @@
 
 namespace dlds\components\validators\isocode;
 
+use common\modules\core\models\db\CoreCountry;
+use yii\base\UnknownPropertyException;
+
 class PhoneNumber extends ISOValidator
 {
+    const DEFAULT_REGION = 'CZ';
+
 
     /**
-     * @param mixed $value
-     *
-     * @return bool
+     * @inheritdoc
      */
-    public function validateValue($value)
+    public function validateAttribute($model, $attribute)
     {
+
+        $codeAttribute = sprintf("%s_%s", $attribute, 'code');
+        $defaultRegion = self::DEFAULT_REGION;
+
+        try {
+            $coreCountryId = $model->$codeAttribute;
+            $coreCountryRow = CoreCountry::findOne($coreCountryId);
+            if ($coreCountryRow) {
+                $defaultRegion = $coreCountryRow->iso;
+            }
+        } catch (UnknownPropertyException $e) {
+
+        }
+
         $phoneUtil = \libphonenumber\PhoneNumberUtil::getInstance();
 
         try {
-            $number = $phoneUtil->parse($value, 'CZ');
+            $number = $phoneUtil->parse($model->$attribute, $defaultRegion);
 
             if ($phoneUtil->isValidNumber($number)) {
-                return true;
+                $isValid = true;
             } else {
-                return false;
+                $isValid = false;
             }
         } catch (\Exception $ex) {
-            return false;
+            $isValid = false;
+        }
+
+        if (!$isValid) {
+            $this->addError($model, $attribute, $this->message);
         }
     }
+
 
 }
